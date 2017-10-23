e1e835f3f5f2f3dbc1ebb43416c7b206753c70ca
annejsize <annejsize@gmail.com>
2017-10-22 20:39:56 -0500

    worked on the logic. it's a big nope so far

--- game.js
+++ game.js
@@ -16,7 +16,6 @@ var database = firebase.database();
 // ------------------------------------
 // Initial Values
 
-
 var playOneName = " ";
 var playTwoName = " ";
 var counter = 0;
@@ -28,58 +27,87 @@ $("#name-submit").on("click", function(event) {
   event.preventDefault();
 
 if (counter === 0) {
-      counter++;
-      name = $("#player-name").val().trim();
-      var playOneWin = 0;
-      var playOneLoss = 0;
+  console.log(counter);
+  counter++;
+  name = $("#player-name").val().trim();
+  var playOneWin = 0;
+  var playOneLoss = 0;
+  database.ref().push({
+    name: name,
+    onewin: playOneWin,
+    oneloss: playOneLoss,
+    dateadded: firebase.database.ServerValue.TIMESTAMP
+    });
+  return false;
+    }
 
-      // Code for the push
-      database.ref().push({
-        name: name,
-        onewin: playOneWin,
-        oneloss: playOneLoss,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
+  else if (counter === 1) {
+    console.log(counter);
+    counter++;
+    name = $("#player-name").val().trim();
+    var playTwoWin = 0;
+    var playTwoLoss = 0;
+    database.ref().push({
+      name: name,
+      twowin: playTwoWin,
+      twoloss: playTwoLoss,
+      dateadded: firebase.database.ServerValue.TIMESTAMP
+  });
+  name=name;
+  twoWin=playTwoWin;
+  twoLoss=playTwoLoss;
+}
+});
 
- name=name;
- oneWin=playOneWin;
- oneLoss=playOneLoss;
+var connectionsRef = database.ref("/connections");
 
-  // Add name, scores, and icons to player one sreen
-  $("#playername-1").text("hi, " + name);
-  $("#one-score").text("Wins: " + oneWin + " Losses: " + oneLoss);
+// '.info/connected' is a special location provided by Firebase that is updated
+// every time the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
+
+// When the client's connection state changes...
+connectedRef.on("value", function(snap) {
+
+  // If they are connected..
+  if (snap.val()) {
+
+    // Add user to the connections list.
+    var con = connectionsRef.push(true);
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
+  }
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+console.log(counter);
+ if (counter === 0) {
+  // full list of items to the well
+  $("#playername-1").text("hi, " + childSnapshot.val().name);
+  $("#one-score").text("Wins: " + childSnapshot.val().onewin + " Losses: " + childSnapshot.val().oneloss);
   $("#image-holder1 h4").text('');
   $('#image-holder1').append('<img id="paper1" src="assets/images/paper.png">');
   $('#image-holder1').append('<img id="paper1" src="assets/images/rock.png">');
   $('#image-holder1').append('<img id="paper1" src="assets/images/scissors.png">');
   $("name-submit").text('');
-} else if (counter ===1) {
+}
+else if (counter === 1) {
 
-  counter++;
-  name = $("#player-name").val().trim();
-  var playTwoWin = 0;
-  var playTwoLoss = 0;
-
-  // Code for the push
-  database.ref().push({
-    name: name,
-    twowin: playTwoWin,
-    twoloss: playTwoLoss,
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
-  });
-
-  name=name;
-  twoWin=playTwoWin;
-  twoLoss=playTwoLoss;
-
-  $("#playername-2").text("hi, " + name);
-  $("#two-score").text("Wins: " + twoWin + " Losses: " + twoLoss);
+  $("#playername-2").text("hi, " + childSnapshot.val().name);
+  $("#two-score").text("Wins: " + childSnapshot.val().twoWin + " Losses: " + childSnapshot.val().twoLoss);
   $("#image-holder2 h4").text('');
   $("#image-holder2").append('<img id="paper2" src="assets/images/paper.png">');
   $("#image-holder2").append('<img id="paper2" src="assets/images/rock.png">');
   $("#image-holder2").append('<img id="paper2" src="assets/images/scissors.png">');
   $("#status-update h4").text("Player One, you're up! Select paper, rock, or scissors");
 }
+else {
+  alert("you fucked up");
+}
+
+// Handle the errors
+}, function(errorObject) {
+  console.log("Errors handled: " + errorObject.code);
 });
 
 //When update to the Firebase server takes place, update accordingly
